7f2b6a2e04e486cac61ae0327431bbfddbe5614b
Oscar Mora <ozzqarmora@gmail.com>
2022-10-28 23:45:04 -0500

    interpolacion

--- Readme.md
+++ Readme.md
@@ -1,8 +1,15 @@
-en está branch se trabajó la escructura basica de vue.js
-Vue.createApp({
-        data(){
-          return{
-            text:'Hola Mundo desde vue!'
-          };
-        }
-      }).mount('#app');
+En está branch se estuda la interponacion de datos en Vue.js y  directivas
+
+{{Siempre debemos buscar poner la lógica en *.js files}}
+
+# INTERPONACION
+La forma más básica de enlace de datos es la interpolación de texto utilizando la sintaxis "Bigote" (llaves dobles):
+<span>Message: {{ msg }}</span>
+
+# TEMPLATES
+Como si fuera html directo, nos permite indicar a la vista del componente, declarativamente poner variables de js usando la sintaxis de templates
+por ejemplo 
+
+las directivas 
+
+y evitar hacer logica directo donde estámos montando nuestras aplicaciones
--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const { createApp } = Vue;
       const vm= createApp({
         data() {
           return {
-            message: "Esto es un mensaje que se va a meter en un template! y se está imprimiendo",
+            message: "<p>Esto es un mensaje que se va a meter en un template! y se está imprimiendo sin tocar el HTML pero no renderiza la tag p </p>",
+            message1: "<p>Este mensaje se está imprimiendo usando v-html <br> deberíamos ser atentos de no usar NUNCA en inputs que usen los usuarios, como buen practica </p>",
             name:'Bitcoin'
         };
     },  //LAS DIRECTIVAS SE AGREGAN COMO ATRIBUTOS A LOS TAGS DE HTML
     //v-text es un adirectiva para pasarle argumentos DE textContent de js
-    template:`<div v-text="message"> </div>`,
+//<--usando la directiva traemos desde la variable text
+     template:`<div v-text="message"> </div>
+     <br><div v-once v-text="name"> </div>
+     <br><div v-html="message1"> </div>`,//v-once se ocupa para poder renderizar solo una vez el de la primera vez
+
     // jugo:`<div v-text="name"></div>`,
     // template1:`<div v-html="jugo"></div>`
         
